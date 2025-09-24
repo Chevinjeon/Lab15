fadf100795758e116a66c717a7a3b4e85f25a03b
Chevin Jeon <109643560+Chevinjeon@users.noreply.github.com>
2025-09-23 19:41:38 -0700

    second commit

--- movieclass.cpp
+++ movieclass.cpp
@@ -23,3 +23,43 @@
  *     - Prints all movies in formatted style
  * ---------------------------------------------------------
  */
+
+
+#include <iostream>
+#include <fstream>   // file I/O
+#include <string>
+#include <vector>    // container for movies
+#include <stdexcept>
+
+class Movie {
+private:
+    std::string title;
+    int yearReleased;
+    std::string screenWriter;
+
+public:
+    // Default constructor
+    Movie() : title(""), yearReleased(0), screenWriter("") {}
+
+    // Parameterized constructor
+    Movie(const std::string &t, int year, const std::string &writer)
+        : title(t), yearReleased(year), screenWriter(writer) {}
+
+    // ---- Setters ----
+    void setTitle(const std::string &t) { title = t; }
+    void setYearReleased(int y) { yearReleased = y; }
+    void setScreenWriter(const std::string &w) { screenWriter = w; }
+
+    // ---- Getters ----
+    std::string getTitle() const { return title; }
+    int getYearReleased() const { return yearReleased; }
+    std::string getScreenWriter() const { return screenWriter; }
+
+    // ---- Print method ----
+    void print() const {
+        std::cout << "Movie: " << screenWriter << std::endl;
+        std::cout << "    Year released: " << yearReleased << std::endl;
+        std::cout << "    Screenwriter: " << title << std::endl;
+        std::cout << std::endl;
+    }
+};
